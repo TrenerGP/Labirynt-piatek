24310d3a0c9d521ad492da0d472020100844d4fc
TrenerGP <kamieniarz.work@gmail.com>
2025-08-08 16:40:05 +0200

    Lekcja29

---
+++ Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour
+{
+    public Texture2D map;
+    public ColorToPrefab[] colorMappings;
+    public float offset = 5f;
+
+    void GenerateTile(int x, int z)
+    {
+
+        Color pixelColor = map.GetPixel(x, z);
+        if (pixelColor.a == 0)
+        {
+            return;
+        }
+        foreach (ColorToPrefab colorMapping in colorMappings)
+        {
+
+            if (colorMapping.color.Equals(pixelColor))
+            {
+                Vector3 position = new Vector3(x, 0, z) * offset;
+                Instantiate(colorMapping.prefab, position, Quaternion.identity, transform);
+            }
+        }
+    }
+
+    public void GenerareLabirynth()
+    {
+        for (int x=0; x<map.width; x++)
+        {
+            for (int z=0; z<map.height; z++)
+            {
+                GenerateTile(x, z);
+            }
+        }
+    }
+}
